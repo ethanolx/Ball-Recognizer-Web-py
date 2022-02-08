6f68338c08a5fdc2b78a2d99e4dc6945366a4ee4
Ethan Tan <imotekhx@gmail.com>
2022-02-08 18:26:54 +0800

    renamed wireframes;deleted empty wireframe;created simple deployment script

--- clear_cache.py
+++ clear_cache.py
@@ -2,10 +2,15 @@ import os
 import glob
 import shutil
 
-shutil.rmtree('application/database.db', ignore_errors=True)
+try:
+    os.remove('application/database.db')
+except:
+    pass
+finally:
+    for f in glob.glob('application/images/*.png'):
+        os.remove(f)
 
-for f in glob.glob('application/images/*.png'):
-    os.remove(f)
+    for f in glob.glob('**/__pycache__', recursive=True):
+        shutil.rmtree(f, ignore_errors=True)
 
-for f in glob.glob('**/__pycache__', recursive=True):
-    shutil.rmtree(f, ignore_errors=True)
+    shutil.rmtree('.pytest_cache', ignore_errors=True)
